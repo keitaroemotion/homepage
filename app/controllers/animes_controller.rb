b4a1c1f9a250aad81aaef97cbbb320a5ce1ed920
Kei Sugano <tobasojyo@gmail.com>
2019-04-17 18:28:07 +0900

    minor fix

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,14 +1,10 @@
 class AnimesController < ApplicationController
+
   def index
     I18n.locale = lang if lang
 
-    sequence_dir = "sequences"
-    gif_dir      = "gif"
-
     @animes = Dir["app/assets/images/animes/*"].map { |dir|
-      sequences = expand(File.join(dir, sequence_dir))
-      gif       = expand(File.join(dir, gif_dir)).first.gsub("app/assets/images/", "")
-      [sequences, gif]
+      seq_and_gif(dir)
     }
   end
 
@@ -18,5 +14,17 @@ class AnimesController < ApplicationController
 
   def lang
     super
+  end
+
+  private
+
+  def seq_and_gif(dir)
+    zip = File.join(zip_dir, File.basename(dir) +".zip").gsub("public/", "")
+    gif = expand(File.join(dir, "gif")).first.gsub("app/assets/images/", "")
+    [zip, gif]
+  end
+
+  def zip_dir
+    "public/animes"
   end
 end
